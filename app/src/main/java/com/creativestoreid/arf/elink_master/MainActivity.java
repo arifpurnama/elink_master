19ff93834dcfacd2ea777dd1ee1b005e371e908e
arifpurnama <35787360+arifpurnama@users.noreply.github.com>
2019-11-26 21:48:30 +0700

    Update Menu and SetOnClick

--- app/src/main/java/com/creativestoreid/arf/elink_master/MainActivity.java
+++ app/src/main/java/com/creativestoreid/arf/elink_master/MainActivity.java
@@ -2,7 +2,13 @@ package com.creativestoreid.arf.elink_master;
 
 import android.os.Bundle;
 
-import com.creativestoreid.arf.elink_master.ui.home.HomeFragment;
+import com.creativestoreid.arf.elink_master.fragments.About;
+import com.creativestoreid.arf.elink_master.fragments.HomeFragment;
+import com.creativestoreid.arf.elink_master.fragments.InformasiUmum;
+import com.creativestoreid.arf.elink_master.fragments.LaporanWarga;
+import com.creativestoreid.arf.elink_master.fragments.PendataanTempatTinggal;
+import com.creativestoreid.arf.elink_master.fragments.PendataanWarga;
+import com.creativestoreid.arf.elink_master.fragments.PendataanWilayah;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -11,7 +17,9 @@ import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -31,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private AppBarConfiguration mAppBarConfiguration;
     private Object HomeFragment;
+    private View navHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,18 +56,46 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         .setAction("Action", null).show();
             }
         });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navHeader = navigationView.getHeaderView(0);
+
+        //Loading profile image
+
+        //Loading background Image
+
+        //Select Home by default
+        navigationView.setCheckedItem(R.id.nav_home);
+        Fragment fragment = new HomeFragment();
+        displayselectedFragment(fragment);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_pendataanwarga, R.id.nav_pendataanwilayah,
-                R.id.nav_pendataantempattinggal, R.id.nav_informasiumum, R.id.nav_laporanwarga, R.id.nav_about)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+//        mAppBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.nav_home, R.id.nav_pendataanwarga, R.id.nav_pendataanwilayah,
+//                R.id.nav_pendataantempattinggal, R.id.nav_informasiumum, R.id.nav_laporanwarga, R.id.nav_about)
+//                .setDrawerLayout(drawer)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.frame);
+//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -67,43 +105,59 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.frame);
+//        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
+
+
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         //Handle Navigation view item click item
         int id = item.getItemId();
-
+        Fragment fragment = null;
         if(id == R.id.nav_home){
-            HomeFragment homeFragment = new HomeFragment();
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.nav_host_fragment, homeFragment);
-            fragmentTransaction.commit();
-
+            fragment = new HomeFragment();
+            displayselectedFragment(fragment);
         }else if(id == R.id.nav_pendataanwarga){
-            PendataanWarga pendataanwarga = new PendataanWarga();
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.nav_host_fragment, pendataanwarga);
-            fragmentTransaction.commit();
-
+            fragment = new PendataanWarga();
+            displayselectedFragment(fragment);
         }else if(id == R.id.nav_pendataanwilayah){
-
+            fragment = new PendataanWilayah();
+            displayselectedFragment(fragment);
         }else if(id == R.id.nav_pendataantempattinggal){
-
+            fragment = new PendataanTempatTinggal();
+            displayselectedFragment(fragment);
         }else if(id == R.id.nav_informasiumum){
-
+            fragment = new InformasiUmum();
+            displayselectedFragment(fragment);
         }else if(id == R.id.nav_laporanwarga){
-
+            fragment = new LaporanWarga();
+            displayselectedFragment(fragment);
         }else if(id == R.id.nav_about){
-
+            fragment = new About();
+            displayselectedFragment(fragment);
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void displayselectedFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
     }
 }
